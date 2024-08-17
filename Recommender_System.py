30af576a288b32a0fbfca04f45696620d2d53ce6
congluat03 <103414162+congluat03@users.noreply.github.com>
2024-08-18 01:18:36 +0700

    Update Recommender_System.py

--- Recommender_System.py
+++ Recommender_System.py
@@ -151,7 +151,7 @@ elif choice == 'Collaborative Prediction':
     # Theo cách cho người dùng chọn khách sạn từ dropdown
     # Tạo một tuple cho mỗi khách sạn, trong đó phần tử đầu là tên và phần tử thứ hai là ID
     hotel_options = [(row['Reviewer_Name'], row['New_ID']) for index, row in st.session_state.random_hotels.iterrows()]
-    st.session_state.random_hotels_comments
+    st.session_state.random_hotels
     # Tạo một dropdown với options là các tuple này
     selected_hotel = st.selectbox(
         "Chọn khách sạn",
