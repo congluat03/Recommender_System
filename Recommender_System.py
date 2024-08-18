00d5a477078476f92f96dcd9b680f227a116609f
congluat03 <103414162+congluat03@users.noreply.github.com>
2024-08-19 03:48:51 +0700

    Update Recommender_System.py

--- Recommender_System.py
+++ Recommender_System.py
@@ -88,7 +88,7 @@ elif choice == 'Build Project':
     st.write("##### 3. Build model...")
     st.write("##### 4. Evaluation")
     start_time = time.time()
-     recommendations = get_recommendations(df_hotels, '1_1', cosine_sim=cosine_sim_new, nums=3) 
+    recommendations = get_recommendations(df_hotels, '1_1', cosine_sim=cosine_sim_new, nums=3) 
     print("Thời gian chạy Cosine: %s seconds" % (time.time() - start_time))
     st.subheader("#Collaborative")
     st.write("##### 1. Some data")
@@ -99,7 +99,7 @@ elif choice == 'Build Project':
     st.write("##### 3. Build model...")
     st.write("##### 4. Evaluation")
     start_time = time.time()
-     recommendations = get_recommendations(df_hotels, '1_1', cosine_sim=cosine_sim_new, nums=3) 
+    recommendations = get_recommendations(df_hotels, '1_1', cosine_sim=cosine_sim_new, nums=3) 
 elif choice == 'Content-based prediction':
     st.subheader("Content-based prediction")
     # Lấy 10 khách sạn
