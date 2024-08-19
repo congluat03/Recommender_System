d8577cbd9e919e4bb79f69561b6e6cb4a136db46
congluat03 <103414162+congluat03@users.noreply.github.com>
2024-08-19 16:37:53 +0700

    Update Recommender_System.py

--- Recommender_System.py
+++ Recommender_System.py
@@ -215,6 +215,8 @@ elif choice == 'Build Project':
     
     results = cross_validate(SVD_Surprise, data, measures=['RMSE', 'MAE'], cv=5, return_train_measures=True, verbose=True)
     results_df1 = pd.DataFrame.from_dict(results).mean(axis=0)
+    results_df1 = results_df1[['train_rmse', 'test_rmse', 'train_mae', 'test_mae', 'fit_time', 'test_time']]
+    results_df.columns = ['train_rmse', 'test_rmse', 'train_mae', 'test_mae', 'fit_time', 'test_time']
     results_df = pd.DataFrame.from_dict(results)
     results_df = results_df[['train_rmse', 'test_rmse', 'train_mae', 'test_mae']]
     results_df.columns = ['Train RMSE', 'Test RMSE', 'Train MAE', 'Test MAE']
